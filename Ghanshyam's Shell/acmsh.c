bb3892602945900005769d0fec9858bd8dbc34e0
jarvis-001 <61859855+jarvis-001@users.noreply.github.com>
2021-04-10 23:06:04 +0530

    Update acmsh.c

--- Ghanshyam's Shell/acmsh.c
+++ Ghanshyam's Shell/acmsh.c
@@ -10,7 +10,7 @@
  
 int shell_cd(char** args)
 {
-	if(args[1]==NULL)	fprintf(stderr,"shell: expected argument to \"cd\"\n");
+	if(args[1]==NULL)	fprintf(stderr," shell: expected argument to \"cd\"\n");
 	else
 	{
 		if(chdir(args[1])!=0)  perror("Invalid Call");
